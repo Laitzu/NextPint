cf0728c42ecf28c14c0ac1397d3c99bad4f834a7
Saku Tarkiainen <saku.tarkiainen@gmail.com>
2026-08-28 15:13:25 +0300

    Added file input button to page

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello, TypeScript world!");
-});
+const uploadButton = document.getElementById("upload-button");
+
+uploadButton?.addEventListener("click", handleFileUpload);
+
+function handleFileUpload() {
+    const input = document.getElementById("file-input") as HTMLInputElement;
+    const file = input.files?.[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+    
+    reader.onload = (e) => {
+        const content = e.target?.result as string;
+        console.log(content);
+    };
+    reader.readAsText(file);
+}
