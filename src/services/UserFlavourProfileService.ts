175f8d5e1d67f6cfe552dfce410aae8ae0a82b16
Saku Tarkiainen <saku.tarkiainen@gmail.com>
2026-08-30 13:15:34 +0300

    UserFlavourProfileService initialization and plan

---
+++ src/services/UserFlavourProfileService.ts
@@ -0,0 +1,5 @@
+// Analysis of user untappd drinks and simple flavour profile goes here
+
+// The flavour profile could use things like top 3 beer styles (e.g. IPA, Pale Lager, Stout) in the beginning
+
+// The ABV could also be used to determine some sort of simple preference (Low, Medium, High).
